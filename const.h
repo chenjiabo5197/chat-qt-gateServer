5dabe3cb37499a0cd5b077f0c8970e542cc05117
chenjiabo <chen_wangyi666@163.com>
2024-12-16 23:33:06 +0800

    添加redis库

--- const.h
+++ const.h
@@ -19,6 +19,7 @@
 #include <queue>
 #include <mutex>
 #include <condition_variable>
+#include "hiredis.h"
 
 namespace beast = boost::beast;         // from <boost/beast.hpp>
 namespace http = beast::http;           // from <boost/beast/http.hpp>
